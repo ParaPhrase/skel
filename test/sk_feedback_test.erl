642cd1de1873b7a2fb7355c18973bbb99911d512
mpm <mpmiszczyk@gmail.com>
2013-08-29 14:27:17 +0200

    TEST: fixed up some misplacement of tests.

--- test/sk_feedback_test.erl
+++ test/sk_feedback_test.erl
@@ -6,13 +6,9 @@
 -compile(export_all).
 
 
-simple_example_of_decomp_usage_test()->
-    ?assertSameDataInside( [ {0,3,6},
-                             {9,12,15} ],
-                           skel:do( [{ decomp,
-                                       [ fun (X) -> X*3 end],
-                                       fun erlang:tuple_to_list/1,
-                                       fun erlang:list_to_tuple/1}] ,
-                                    [ {0,1,2},
-                                      {3,4,5} ] )).
-
+simple_example_of_feedback_usage_test()->
+    ?assertSameDataInside( [ 5,5,5,5,5,6,7,8,9,10 ],
+                           skel:do( [{ feedback,
+                                       [ fun (X) -> X+1 end],
+                                       fun (X) -> X<5 end }] ,
+                                    [ 0,1,2,3,4,5,6,7,8,9,10 ] )).
