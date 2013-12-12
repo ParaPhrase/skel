265cc2ea669b36be568c37e9bb60064dc1bf07a3
mpm <mpmiszczyk@gmail.com>
2013-12-12 20:04:07 +0100

    Moved feedback test to proper module.

--- test/sk_map_test.erl
+++ test/sk_map_test.erl
@@ -6,10 +6,4 @@
 -compile(export_all).
 
 
-simple_example_of_feedback_usage_test()->
-    ?assertSameDataInside( [ 5,5,5,5,5,6,7,8,9,10 ],
-                           skel:do( [{ feedback,
-                                       [ fun (X) -> X+1 end],
-                                       fun (X) -> X<5 end }] ,
-                                    [ 0,1,2,3,4,5,6,7,8,9,10 ] )).
 
