786798ed54638304ba9a054b109c1e3da5fbffdf
mpm <mpmiszczyk@gmail.com>
2013-12-20 19:06:02 +0100

    Specs and documentation update.

--- src/sk_ord.erl
+++ src/sk_ord.erl
@@ -31,13 +31,12 @@
 
 -include("skel.hrl").
 
--ifdef(TEST).
--compile(export_all).
--endif.
 
--spec start(workflow() , pid() ) -> maker_fun().
+
 %% @doc Constructs an Ord skeleton wrapper to ensure the outputs of the 
 %% specified workflow are in the same order as that of its inputs.
+-spec start(workflow() , pid() ) ->
+               pid().
 start({WorkFlow}, NextPid ) ->
   ReordererPid = spawn(sk_ord_reorderer, start, [NextPid]),
   WorkerPid = sk_utils:start_worker(WorkFlow, ReordererPid),
